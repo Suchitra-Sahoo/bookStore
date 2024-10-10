dfc45bf516513a51687ae34df9c1e9507c43b124
Suchitra Sahoo <suchitrasahoo874@gmail.com>
2024-10-10 12:22:45 +0530

    updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,23 +10,19 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-const PORT=process.env.PORT || 4001;
+const PORT = process.env.PORT || 4001;
 const URI = process.env.MONGODB_URI;
-//connect to mongoDB
-try{
-   mongoose.connect(URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-   })
-   console.log("connected to mongoDB")
-}catch(error){
-console.log("error:",error)
-}
-//defining routes
-app.use("/book", bookRoute)
-app.use("/user", userRoute)
 
+// Connect to MongoDB
+mongoose.connect(URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log("MongoDB connection error:", error));
 
+// Defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
